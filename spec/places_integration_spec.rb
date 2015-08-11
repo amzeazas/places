921b30504b44e540cf77117e295b3b02c2e29002
Alexa Zeazas & Rachael Berecka <student@epicodus.com>
2015-08-11 11:17:10 -0700

    Correct type in places_spec.rb file.

--- spec/places_integration_spec.rb
+++ spec/places_integration_spec.rb
@@ -6,7 +6,7 @@ set(:show_exceptions, false)
 describe('the path of places', {:type =>:feature}) do
   it('processes the user entry and adds it to a list of places') do
     visit('/')
-    fill_in('places', :with => "Cardiff, Wales")
+    fill_in('location', :with => "Cardiff, Wales")
     click_button('Add Place')
     expect(page).to have_content('Your place has been successfully added.')
   end
--- spec/places_spec.rb
+++ spec/places_spec.rb
@@ -2,11 +2,9 @@ require('rspec')
 require('places')
 
 describe(Places) do
-  describe(Places) do
     before() do
       Places.clear()
     end
-  end
 
   describe("#location") do
     it("lets you give it a location") do
@@ -14,11 +12,13 @@ describe(Places) do
       expect(test_place.location()).to(eq("Berlin, Germany"))
     end
   end
+
   describe(".all") do
     it("is empty at first") do
       expect(Places.all()).to(eq([]))
     end
   end
+
   describe("#save") do
     it("adds a location to the array of saved locations") do
       test_place = Places.new("Phrae, Thailand")
@@ -26,6 +26,7 @@ describe(Places) do
       expect(Places.all()).to(eq([test_place]))
     end
   end
+
   describe(".clear") do
     it("empties out all of the saved places") do
       Places.new("Phrae, Thailand").save()
