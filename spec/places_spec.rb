8304524e50e597d8dd5b071093fea3b527219aa7
Alexa Zeazas & Rachael Berecka <student@epicodus.com>
2015-08-11 10:29:00 -0700

    Add location method spec.

--- spec/places_spec.rb
+++ spec/places_spec.rb
@@ -1,1 +1,11 @@
+require('rspec')
+require('task')
 
+describe(Places) do
+  describe("#location") do
+    it("lets you give it a location") do
+      test_place = Places.new("Berlin, Germany")
+      expect(test_place.description()).to(eq("Berlin, Germany"))
+    end
+  end
+end
